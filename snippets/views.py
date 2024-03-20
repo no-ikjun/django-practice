99c311f4957599f6ab5c4dc39e5ef2439d424e8d
ikjunchoi <choiikjun1101@gmail.com>
2024-03-20 10:18:13 +0900

    🎨 update: using generic class-based views

--- snippets/views.py
+++ snippets/views.py
@@ -1,45 +1,17 @@
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-
-from rest_framework import status
-from rest_framework.views import APIView
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.response import Response
-from rest_framework import permissions
-from rest_framework import mixins
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 
-class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+
+class SnippetList(generics.ListCreateAPIView):
     permission_classes = [AllowAny]
-    """
-    List all snippets, or create a new snippet.
-    """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class SnippetDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [AllowAny]
-    """
-    Retrieve, update or delete a snippet instance.
-    """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-    
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-    
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
